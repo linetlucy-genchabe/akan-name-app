8cc76455e26c4e9f5c37236fa30ef49402f63aa9
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-02 11:48:41 +0300

    add months array

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,4 @@ var dayOfWeekNumbers = (day+(2.6*month-0.2)-2*century+year+(year/4)+(century/4))
 console.log("day of week number:",dayOfWeekNumber);
 
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const months= ["January","February","March","April","May","June","July","August","September","October","November","December"];
