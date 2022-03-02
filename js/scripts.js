407e1e2e455aa65b194368b79d42acb2dbd4352a
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-02 11:47:47 +0300

    add weekday array

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,5 @@ var gender= prompt("gender: F/M");
 var dayOfWeekNumbers = (day+(2.6*month-0.2)-2*century+year+(year/4)+(century/4)) % 7;
 
 console.log("day of week number:",dayOfWeekNumber);
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
