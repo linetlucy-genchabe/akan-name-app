b04b3e4229de7145336f0eaedaa925a38d68c3cb
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-02 11:46:41 +0300

    add formula to derive day of week number

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ var gender;
 var akanNameMale;
 var akanNameFemale;
 
+var gender= prompt("gender: F/M");
+var dayOfWeekNumbers = (day+(2.6*month-0.2)-2*century+year+(year/4)+(century/4)) % 7;
+
+console.log("day of week number:",dayOfWeekNumber);
