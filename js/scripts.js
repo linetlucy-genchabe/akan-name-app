bedfe6010c603f5ec96e36336c9710be6bbcb681
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-03 13:47:23 +0300

    add all updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,117 +1,78 @@
+var gender = ("Female");
+
 var nameOfDay;
 var gender;
 var akanName;
-var gender;
-function myFunction(){
-    document.getElementById("myGender").innerHTML(id="gender");
-}
-//let gender = prompt("enter gender", "Female/Male");
-// function myFunction()
-// {
-//     var gender;
-//     if(gender=="Male"){
-//         document.getElementById("myGender").innerHTML=gender;
-//     }else if(gender=="Female")
-//     {
-//          document.getElementById("myGender").innerHTML=gender;
-//     };
-// // let gender= document.getElementById("myGender").value;
-// // document.getElementById("genderr").innerHTML=gender;
-// };
-//var dayOfWeekNumbers = (day+(2.6*month-0.2)-2*century+year+(year/4)+(century/4)) % 7;
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-//console.log("day of week number:",dayOfWeekNumber);
-
-
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const months= ["January","February","March","April","May","June","July","August","September","October","November","December"];
-const d = new Date("June 15, 2001");
+const d = new Date("February 27, 2022");
 let day = d.getDate();
-let month= d.getMonth();
+let month = d.getMonth();
 let year = d.getFullYear();
-let dayOfWeekNumber= d.getDay();
-let century= year/100;
+let dayOfWeekNumber = d.getDay();
+let century = year / 100;
 
- var dayOfWeekNumbers = (day+(2.6*month-0.2)-2*century+year+(year/4)+(century/4)) % 7;
+var dayOfWeekNumbers = (day + (2.6 * month - 0.2) - 2 * century + year + (year / 4) + (century / 4)) % 7;
 
-console.log("day of week number:",dayOfWeekNumber);
-console.log("Gender:",gender);
+console.log("day of week number:", dayOfWeekNumber);
+console.log("Gender:", gender);
 
-if (dayOfWeekNumber===0 && gender==="F")
-{
-    console.log( nameOfDay = "DAY OF WEEK:",'Sunday');
-  console.log("Your akan name is:",akanName="Akosua");
+function newFunction() {
+    if (document.getElementById("Female").checked == true) {
+        console.log("your gender is:", gender = "Female");
+    } else if (document.getElementById("Male").checked == true) {
+        console.log("your gender is:", gender = "Male");
+    } else {
+        console.log("invalid");
+    }
 }
-else if(dayOfWeekNumber===0 && gender==="M")
-{
-    console.log( nameOfDay = "DAY OF WEEK:",'Sunday');
-    console.log("Your akan name is:",akanName="kwasi");
-}
-else if(dayOfWeekNumber===1 && gender==="F")
-{
-  console.log( nameOfDay = "DAY OF WEEK:",'Monday');
-  console.log("Your akan name is:",akanName="adwoa");
-}
-else if(dayOfWeekNumber===1 && gender==="M")
-{
-  console.log( nameOfDay = "DAY OF WEEK:",'Monday');
-  console.log("Your akan name is:",akanName="kwadwo");
-}
-else if(dayOfWeekNumber===2 && gender==="F")
-{
-  console.log(nameOfDay = "DAY OF WEEK:",'Tuesday');
-  console.log("Your akan name is:",akanName="abenaa");
-}
-else if(dayOfWeekNumber===2 && gender==="M")
-{
-  console.log(nameOfDay = "DAY OF WEEK:",'Tuesday');
-  console.log("Your akan name is:",akanName="kwabena");
-}
-else if(dayOfWeekNumber===3 && gender==="F")
-{
-  console.log( nameOfDay = "DAY OF WEEK:",'Wednesday');
-  console.log("Your akan name is:",akanName="akua");
-}
-else if(dayOfWeekNumber===3 && gender==="M")
-{
-  console.log( nameOfDay = "DAY OF WEEK:",'Wednesday');
-  console.log("Your akan name is:",akanName="kwaku");
-}
-else if(dayOfWeekNumber===4 && gender==="F")
-{
-   console.log(nameOfDay = "DAY OF WEEK:",'Thursday');
-  console.log("Your akan name is:",akanName="yaa");
-}
-else if(dayOfWeekNumber===4 && gender==="M")
-{
-   console.log(nameOfDay = "DAY OF WEEK:",'Thursday');
-  console.log("Your akan name is:",akanName="yaw");
-}
-else if(dayOfWeekNumber===5 && gender==="F")
-{
-  console.log( nameOfDay = "DAY OF WEEK:",'Friday');
-  console.log("Your akan name is:",akanName="afua");
-}
-else if(dayOfWeekNumber===5 && gender==="M")
-{
-  console.log( nameOfDay = "DAY OF WEEK:",'Friday');
-  console.log("Your akan name is:",akanName="kofi");
-}
-else if(dayOfWeekNumber===6 && gender==="F")
-{
-   console.log(nameOfDay = "DAY OF WEEK:",'Saturday');
-  console.log("Your akan name is:",akanName="ama");
-}
-else if(dayOfWeekNumber===6 && gender==="M")
-{
-   console.log(nameOfDay = "DAY OF WEEK:",'Saturday');
-  console.log("Your akan name is:",akanName="kwame");
-}
-else{
-    console.log("invalid details!");
-  }
-  
-  function newFunction(){
-    var akanName;
-    console.log("The name",akanName);
+
+function myFunction() {
+
+    if (dayOfWeekNumber === 0 && document.getElementById("Female").checked == true) {
+        alert(nameOfDay = "DAY OF WEEK:Sunday");
+        alert("Your akan name is: Akosua");
+    } else if (dayOfWeekNumber === 0 && document.getElementById("Male").checked == true) {
+        alert(nameOfDay = "DAY OF WEEK:Sunday");
+        alert('Your akan name is: "kwasi"');
+    } else if (dayOfWeekNumber === 1 && document.getElementById("Female").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Monday');
+        console.log("Your akan name is:", akanName = "adwoa");
+    } else if (dayOfWeekNumber === 1 && document.getElementById("Male").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Monday');
+        console.log("Your akan name is:", akanName = "kwadwo");
+    } else if (dayOfWeekNumber === 2 && document.getElementById("Female").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Tuesday');
+        console.log("Your akan name is:", akanName = "abenaa");
+    } else if (dayOfWeekNumber === 2 && document.getElementById("Male").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Tuesday');
+        console.log("Your akan name is:", akanName = "kwabena");
+    } else if (dayOfWeekNumber === 3 && document.getElementById("Female").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Wednesday');
+        console.log("Your akan name is:", akanName = "akua");
+    } else if (dayOfWeekNumber === 3 && document.getElementById("Male").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Wednesday');
+        console.log("Your akan name is:", akanName = "kwaku");
+    } else if (dayOfWeekNumber === 4 && document.getElementById("Female").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Thursday');
+        console.log("Your akan name is:", akanName = "yaa");
+    } else if (dayOfWeekNumber === 4 && document.getElementById("Male").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Thursday');
+        console.log("Your akan name is:", akanName = "yaw");
+    } else if (dayOfWeekNumber === 5 && document.getElementById("Female").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Friday');
+        console.log("Your akan name is:", akanName = "afua");
+    } else if (dayOfWeekNumber === 5 && document.getElementById("Male").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Friday');
+        console.log("Your akan name is:", akanName = "kofi");
+    } else if (dayOfWeekNumber === 6 && document.getElementById("Female").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Saturday');
+        console.log("Your akan name is:", akanName = "ama");
+    } else if (dayOfWeekNumber === 6 && document.getElementById("Male").checked == true) {
+        console.log(nameOfDay = "DAY OF WEEK:", 'Saturday');
+        console.log("Your akan name is:", akanName = "kwame");
+    } else {
+        console.log("invalid details!");
+    }
 }
